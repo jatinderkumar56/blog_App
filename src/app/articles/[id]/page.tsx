e11d036138d3a0db074311a39961a1d101dbcd66
Jatinder Kumar <jatinder12kumar12@gmail.com>
2025-06-18 00:00:59 -0800

    Fix dynamic article

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -3,14 +3,15 @@
 import { SingleArticleDetailPage } from "@/components/article/single-article-page";
 import { prisma } from "@/lib/prisma";
 import React from "react";
-
+import { notFound } from "next/navigation"; // use this to trigger 404
 type ArticleDetailPageProps = {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 };
 
 const page: React.FC<ArticleDetailPageProps> = async ({ params }) => {
-  const id = (await params).id;
-  const article = await prisma.articles.findUnique({
+  const id =  params.id;
+  try {
+     const article = await prisma.articles.findUnique({
     where: {
       id,
     },
@@ -27,11 +28,17 @@ const page: React.FC<ArticleDetailPageProps> = async ({ params }) => {
   if (!article) {
     return <h1>Article not found.</h1>;
   }
-  return (
+   return (
     <div>
       <SingleArticleDetailPage article={article} /> 
     </div>
   );
+  } catch (error) {
+    console.error("Error fetching article:", error);
+    notFound(); // triggers 404 on Vercel
+  }
+ 
+ 
 };
 
 export default page;
